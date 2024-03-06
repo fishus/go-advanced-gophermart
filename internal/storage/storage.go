bf892772978719cd440e03fe8f354614887e1fad
Slava Ershov <vyacheslav.ershov@gmail.com>
2024-03-06 20:13:52 +0300

    Rename interfaces

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -8,15 +8,15 @@ import (
 	"github.com/fishus/go-advanced-gophermart/pkg/models"
 )
 
-//go:generate go run github.com/vektra/mockery/v2@v2.42.0 --name=UserStorager  --with-expecter
-type UserStorager interface {
+//go:generate go run github.com/vektra/mockery/v2@v2.42.0 --name=Userer  --with-expecter
+type Userer interface {
 	UserAdd(context.Context, models.User) (models.UserID, error)
 	UserLogin(context.Context, models.User) (models.UserID, error)
 	UserByID(context.Context, models.UserID) (models.User, error)
 }
 
-//go:generate go run github.com/vektra/mockery/v2@v2.42.0 --name=OrderStorager  --with-expecter
-type OrderStorager interface {
+//go:generate go run github.com/vektra/mockery/v2@v2.42.0 --name=Orderer  --with-expecter
+type Orderer interface {
 	OrderAdd(context.Context, models.Order) (models.OrderID, error)
 	OrderByID(context.Context, models.OrderID) (models.Order, error)
 	OrderByFilter(context.Context, ...OrderFilter) (models.Order, error)
@@ -34,7 +34,7 @@ type LoyaltyBalancer interface {
 
 //go:generate go run github.com/vektra/mockery/v2@v2.42.0 --name=Storager  --with-expecter
 type Storager interface {
-	UserStorager
-	OrderStorager
+	Userer
+	Orderer
 	LoyaltyBalancer
 }
