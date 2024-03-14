324e579b391e7fdda249f0f6015bff4a021d492d
Slava Ershov <vyacheslav.ershov@gmail.com>
2024-03-14 22:17:04 +0300

    Fix: add return in main

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -9,22 +9,24 @@ import (
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
-	app.Shutdown(cancel)
+	app.WaitSignalTerm(cancel)
+	defer app.Shutdown(cancel)
 
 	db, err := app.ConnDB(ctx)
 	if err != nil {
 		logger.Log.Error(err.Error())
+		return
 	}
 
 	loyalty, err := app.RunAccrualWorkers(ctx, db)
 	if err != nil {
 		logger.Log.Error(err.Error())
+		return
 	}
 
 	err = app.RunAPIServer(ctx, db, loyalty)
 	if err != nil {
 		logger.Log.Error(err.Error())
+		return
 	}
-
-	<-ctx.Done()
 }
--- internal/app/shutdown.go
+++ internal/app/shutdown.go
@@ -6,12 +6,16 @@ import (
 	"os"
 	"os/signal"
 	"slices"
+	"sync"
 	"syscall"
 
 	"github.com/fishus/go-advanced-gophermart/internal/logger"
 )
 
-var Closers []io.Closer
+var (
+	Closers      []io.Closer
+	onceShutdown sync.Once
+)
 
 func CloseClosers() {
 	slices.Reverse(Closers)
@@ -26,13 +30,19 @@ func CloseClosers() {
 
 // Shutdown implements graceful app
 
-func Shutdown(cancel context.CancelFunc) {
+func WaitSignalTerm(cancel context.CancelFunc) {
 	// Listen for syscall signals for process to interrupt/quit
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	go func() {
 		<-sig
+		Shutdown(cancel)
+	}()
+}
+
+func Shutdown(cancel context.CancelFunc) {
+	onceShutdown.Do(func() {
 		CloseClosers()
 		cancel()
-	}()
+	})
 }
